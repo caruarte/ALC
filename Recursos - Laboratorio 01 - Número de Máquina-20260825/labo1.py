12578911145b86dc707096018842d5e142f5e9c7
César Ruarte <caruarte@gmail.com>
2026-09-01 13:37:53 -0300

    una parte del labo 1

--- Recursos - Laboratorio 01 - Número de Máquina-20260825/labo1.py
+++ Recursos - Laboratorio 01 - Número de Máquina-20260825/labo1.py
@@ -5,6 +5,7 @@ print(format(0.1, '.20f'))
 x = 0.1 + 0.1 + 0.1
 y = 0.3
 
+# ej 1
 print(x == y)
 
 print(format(x, '.20f'), format(y, '.20f'))
@@ -14,7 +15,4 @@ a = 1.0
 while a > 0.1:
     print(a)
     a = a - 0.1 # el problema esta en esta resta de aca, se podria arreglar cambiando el != por un mayor
-
-
-    
 print('fin')
